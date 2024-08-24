c68e5f30d199c7c0730c75273709889011f8a824
samuelamo001 <samuel.amo@amalitechtraining.org>
2024-08-24 08:36:26 +0000

    modification done

--- web-security/src/main/java/com/springsecuritytutorial/tutorial/WebSecurityFundamentals.java
+++ web-security/src/main/java/com/springsecuritytutorial/tutorial/WebSecurityFundamentals.java
@@ -1,15 +1,9 @@
 package com.springsecuritytutorial.tutorial;
 
-import com.springsecuritytutorial.tutorial.security.configuration.RsaKeyProperties;
-import com.springsecuritytutorial.tutorial.service.BookService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
-@EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 public class WebSecurityFundamentals {
 
--- web-security/src/main/java/com/springsecuritytutorial/tutorial/security/SecurityConfig.java
+++ web-security/src/main/java/com/springsecuritytutorial/tutorial/security/SecurityConfig.java
@@ -1,6 +1,5 @@
-package com.springsecuritytutorial.tutorial.security.configuration;
+package com.springsecuritytutorial.tutorial.security;
 import com.springsecuritytutorial.tutorial.security.jwt.JwtAuthenticationFilter;
-import com.springsecuritytutorial.tutorial.security.service.AppUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
