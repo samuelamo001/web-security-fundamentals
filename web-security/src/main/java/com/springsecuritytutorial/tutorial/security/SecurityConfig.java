3d03877da194f68857a34ce566a8948eecc6eb7a
samuelamo001 <samuel.amo@amalitechtraining.org>
2024-08-24 20:13:25 +0000

    added csrf functionalities

--- web-security/src/main/java/com/springsecuritytutorial/tutorial/security/SecurityConfig.java
+++ web-security/src/main/java/com/springsecuritytutorial/tutorial/security/SecurityConfig.java
@@ -1,20 +1,12 @@
 package com.springsecuritytutorial.tutorial.security;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-
-import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -22,14 +14,22 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+
         return http
                 .csrf(csrf-> csrf
                         .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 )
-
                 .authorizeHttpRequests( authorizeRequests -> authorizeRequests
                         .requestMatchers("/login").permitAll()
                         .anyRequest().authenticated()
+                )
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+                        .sessionFixation().migrateSession()
+                        .maximumSessions(1)
+                        .maxSessionsPreventsLogin(true)
+
                 )
                 .oauth2Login(oauth2Login -> oauth2Login
                             .loginPage("/login")
