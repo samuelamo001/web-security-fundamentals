9ca3725c189e4bcd9614360a88ba0d21ab962fe1
samuelamo001 <samuel.amo@amalitechtraining.org>
2024-08-25 17:26:12 +0000

    added

--- web-security/src/main/java/com/springsecuritytutorial/tutorial/WebSecurityFundamentals.java
+++ web-security/src/main/java/com/springsecuritytutorial/tutorial/WebSecurityFundamentals.java
@@ -1,7 +1,10 @@
 package com.springsecuritytutorial.tutorial;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WebSecurityFundamentals {
