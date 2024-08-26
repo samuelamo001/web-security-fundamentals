f1b39bf1118121eb1021522a498942f30201d58c
samuelamo001 <samuel.amo@amalitechtraining.org>
2024-08-26 12:21:00 +0000

    modified filter chain

--- web-security/src/main/java/com/springsecuritytutorial/tutorial/security/SecurityConfig.java
+++ web-security/src/main/java/com/springsecuritytutorial/tutorial/security/SecurityConfig.java
@@ -17,29 +17,40 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
+        configureCsrf(http);
+        configureAuthorization(http);
+        configureSessionManagement(http);
+        configureOAuth2Login(http);
 
-        return http
-                .csrf( csrf -> csrf
-                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+        return http.build();
 
-                .authorizeHttpRequests( authorizeRequests -> authorizeRequests
-                        .requestMatchers("/login").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .sessionManagement(sessionManagement -> sessionManagement
-                        .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
-                        .sessionFixation().migrateSession()
-                        .maximumSessions(1)
-                        .maxSessionsPreventsLogin(true)
-
-                )
-                .oauth2Login(oauth2Login -> oauth2Login
-                            .loginPage("/login")
-                            .successHandler((request, response, authentication) -> response.sendRedirect("/profile"))
-                )
+    }
 
 
 
-                .build();
+    private void configureCsrf(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()));
+    }
+
+    private void configureAuthorization(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                .requestMatchers("/login").permitAll()
+                .anyRequest().authenticated());
+    }
+
+    private void configureSessionManagement(HttpSecurity http) throws Exception {
+        http.sessionManagement(sessionManagement -> sessionManagement
+                .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+                .sessionFixation().migrateSession()
+                .maximumSessions(1)
+                .maxSessionsPreventsLogin(true));
+    }
+
+    private void configureOAuth2Login(HttpSecurity http) throws Exception {
+        http.oauth2Login(oauth2Login -> oauth2Login
+                .loginPage("/login")
+                .successHandler((request, response, authentication) -> response.sendRedirect("/profile"))
+                .failureUrl("/login?error"));
     }
 }
